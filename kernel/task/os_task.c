ed0db62f014e9c2d203a4e827441f9ffa9a422fa
Leo <790754967@qq.com>
2026-06-07 01:56:13 +0800

    refactor(task): remove status checks in OsTaskExit
    Running task cannot have IN_DELAY or PENDING status - those only exist when task is switched out

--- kernel/task/os_task.c
+++ kernel/task/os_task.c
@@ -77,19 +77,6 @@ static OS_SEC_KERNEL_TEXT void OsTaskExit(void)
     struct OsTaskCb *tsk = OS_RUNNING_TASK();
 
     OsSchedRdyListDequeTsk(tsk);
-
-    if (tsk->status & OS_TASK_STATUS_IN_DELAY) {
-        OsListRemoveNode(&tsk->timerListNode);
-    }
-
-    if (tsk->status & OS_TASK_STATUS_PENDING) {
-        OsListRemoveNode(&tsk->pendListNode);
-    }
-
-    if (tsk->tskType == OS_TASK_PROCESS && tsk->pgDir) {
-        /* TODO: 释放进程页目录和用户空间映射 */
-    }
-
     OsMemKernelFree((void *)tsk->kernelStkTop);
 
     tsk->status = 0;
